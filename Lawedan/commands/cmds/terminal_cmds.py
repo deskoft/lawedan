7c80dd1914189a0b26a0ccb3310d91f3f18678a4
deskoft <49288629+deskoft@users.noreply.github.com>
2021-04-06 16:17:09 -0500

    Fixed command to use citizenship terminals.

--- Lawedan/commands/cmds/terminal_cmds.py
+++ Lawedan/commands/cmds/terminal_cmds.py
@@ -1,27 +1,4 @@
 from evennia import Command
-from evennia.utils import evmenu
-
-def citizenship_terminal_start(caller, raw_string, **kwargs):
-    "This is the start of the citizenship terminal."
-
-    def set_last_name(caller, prompt, user_input):
-        caller.set_last_name = user_input.strip()
-
-    def register_citizenship(caller):
-        last_name = get_input(caller, "Write your last name:", set_last_name)
-
-    text = "You approach the citizenship terminal. It presents you with several options."
-
-    options = {}
-
-    if not caller.has_citizenship():
-        options.append({
-            "key":("register", "r"),
-            "desc":"Register as a citizen.",
-            "goto":"citizenship_terminal_start",
-            "exec":register_citizenship
-        })
-
 
 class CmdUseCitizenshipTerminal(Command):
     """
@@ -33,4 +10,4 @@ class CmdUseCitizenshipTerminal(Command):
     key = "use-citizenship-terminal"
     help_category = "terminals"
     def func(self):
-        caller.msg("Working.")
+        self.caller.msg("WORK IN PROGRESS.")
