498d5fd9cb4f11010445e0bbec469c973522d7ff
deskoft <49288629+deskoft@users.noreply.github.com>
2021-04-06 13:16:58 -0500

    First step in a citizenship terminal module.

--- Lawedan/typeclasses/terminals.py
+++ Lawedan/typeclasses/terminals.py
@@ -1,7 +1,6 @@
 from typeclasses.objects import Object
-#from commands.cmdsets.terminal_cmdset import CitizenshipTerminalCmdSet
+from commands.cmdsets.terminal_cmdset import CitizenshipTerminalCmdSet
 
 class CitizenshipTerminal(Object):
-    pass
-#    def at_object_creation(self):
-#        self.cmdset.add(CitizenshipTerminalCmdSet)
+    def at_object_creation(self):
+        self.cmdset.add(CitizenshipTerminalCmdSet)
