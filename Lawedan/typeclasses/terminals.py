5910a32173807d8e8345a302007f58d03a4a1382
deskoft <49288629+deskoft@users.noreply.github.com>
2021-04-06 13:24:06 -0500

    Fixed command to use citizenship terminals.

--- Lawedan/typeclasses/terminals.py
+++ Lawedan/typeclasses/terminals.py
@@ -2,6 +2,6 @@ from typeclasses.objects import Object
 from commands.cmdsets.terminal_cmdset import CitizenshipTerminalCmdSet
 
 class CitizenshipTerminal(Object):
-    locks = "get:perm(Builder)"
     def at_object_creation(self):
+        self.locks.add("get:perm(Builder)")
         self.cmdset.add(CitizenshipTerminalCmdSet)
